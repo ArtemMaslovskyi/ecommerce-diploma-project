dc8a016f13738429f584ed7794d44c34f8932859
Artem Maslovskyi <artemmsloms@gmail.com>
2024-03-10 20:36:21 +0200

    trying to understand parallax

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,30 @@
 import React from "react";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+import background1 from "../pages/background1.png";
+
 export default function About() {
   return (
-    <div>
-      <h2>About</h2>
+    <div className="*:bg-gradient-to-b from-blue-800/80 to-main-yellow/50">
+      <Parallax pages={4}>
+        <ParallaxLayer speed={0.5}>
+          <h2 className="m-6 font-bold text-center text-8xl text-main-yellow">
+            Who are we?
+          </h2>
+          <p className="absolute bottom-0 text-4xl text-center right-2/4 text-white/80">
+            We are trading platform that provides
+          </p>
+        </ParallaxLayer>
+        <ParallaxLayer sticky={{ start: 1, end: 2 }} speed={1}>
+          <div className="absolute flex p-6 rounded-2xl w-fit bg-main-yellow right-2/4">
+            <h2 className="text-3xl font-bold text-slate-100">Trading of:</h2>
+          </div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={1.5}>
+          <div>
+            <h2>fdsf</h2>
+          </div>
+        </ParallaxLayer>
+      </Parallax>
     </div>
   );
 }
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,7 +2,7 @@ import React from "react";
 export default function Contact() {
   return (
     <div>
-      <h2>Contact</h2>
+      <h2>Contact us</h2>
     </div>
   );
 }
