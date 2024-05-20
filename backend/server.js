14f7b7608331c4e21226bac70a1f3e2ee6569c4e
ColonelMelnyk <xiaomiredmia15@gmail.com>
2024-05-20 23:06:01 +0300

    database connection added

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 
 const app = require('./app');
 
-const {CONTACTS_HOST, PORT = 3000} = process.env;
+const {LOTS_HOST, PORT = 3000} = process.env;
 
 mongoose.set('strictQuery', true);
 
-mongoose.connect(CONTACTS_HOST).then(
+mongoose.connect(LOTS_HOST).then(
   ()=>{
     app.listen(PORT, () => { console.log("Database connection successful!")})
 }).catch( error =>{
