b154b762e22a8f2294c18283421214e3db3cc2c3
ColonelMelnyk <xiaomiredmia15@gmail.com>
2024-05-22 14:11:57 +0300

    avatars pt1

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,11 +1,15 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-
+const gravatar = require("gravatar");
+const path = require ("path");
+const fs = require("fs");
 const {User} = require("../models/user");
 
 const { HttpsError, ctrlWrapper } = require("../error_handler");
 
 const {SECRET_KEY} = process.env;
+
+const avatarsDir = path.join(__dirname, "../", "public", "avatars");
 
 const register = async(req, res)=> {
     const {email, password} = req.body;
@@ -16,8 +20,8 @@ const register = async(req, res)=> {
     }
 
     const hashPassword = await bcrypt.hash(password, 10);
-
-    const newUser = await User.create({...req.body, password: hashPassword});
+    const avatarURL = gravatar.url(email);
+    const newUser = await User.create({...req.body, password: hashPassword, avatarURL});
 
     res.status(201).json({
         email: newUser.email,
@@ -70,7 +74,19 @@ const subscription = async(req, res)=>{
         message: "Subscription updated"
     });
 };
+const updateAvatar = async(req, res)=> {
+    const {_id} = req.user;
+    const {path: tempUpload, originalname} = req.file;
+    const filename = `${_id}_${originalname}`;
+    const resultUpload = path.join(avatarsDir, filename);
+    await fs.rename(tempUpload, resultUpload);
+    const avatarURL = path.join("avatars", filename);
+    await User.findByIdAndUpdate(_id, {avatarURL});
 
+    res.json({
+        avatarURL,
+    })
+};
 
 module.exports = {
     register: ctrlWrapper(register),
@@ -78,4 +94,5 @@ module.exports = {
     getCurrent: ctrlWrapper(getCurrent),
     logout: ctrlWrapper(logout),
     subscription: ctrlWrapper(subscription),
+    updateAvatar: ctrlWrapper(updateAvatar),
 }
--- backend/middlewares/index.js
+++ backend/middlewares/index.js
@@ -6,4 +6,5 @@ module.exports = {
     validateBody,
     isValidId,
     authenticate,
+    upload,
 }
