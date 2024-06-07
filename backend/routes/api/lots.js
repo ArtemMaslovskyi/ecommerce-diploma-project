9b2e1d615e82f73b9fbb37586da13bd17fb58ec1
ColonelMelnyk <xiaomiredmia15@gmail.com>
2024-06-07 11:16:21 +0300

    fix gravatar and upload

--- backend/routes/api/lots.js
+++ backend/routes/api/lots.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const ctrl = require("../../controllers/lots");
 
-const {validateBody, isValidId, authenticate, avatarSize} = require("../../middlewares");
+const {validateBody, isValidId, authenticate, avatarSize, upload} = require("../../middlewares");
 
 const {schemas} = require("../../models/lot");
 const router = express.Router();
