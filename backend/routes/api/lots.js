b30ec15249da0e1d9ddecb20a6fce44d32a09bfc
ColonelMelnyk <xiaomiredmia15@gmail.com>
2024-06-22 17:10:28 +0300

    authentication disabled

--- backend/routes/api/lots.js
+++ backend/routes/api/lots.js
@@ -6,17 +6,17 @@ const {validateBody, isValidId, authenticate, avatarSize, upload} = require("../
 const {schemas} = require("../../models/lot");
 const router = express.Router();
 
-router.get("/",  authenticate, ctrl.listLots);
+router.get("/", ctrl.listLots);
 
-router.get("/:id", authenticate, isValidId, ctrl.getLotById);
+router.get("/:id", isValidId, ctrl.getLotById);
 
-router.post("/", authenticate, validateBody(schemas.addSchema), ctrl.addLot);
+router.post("/", validateBody(schemas.addSchema), ctrl.addLot);
 
-router.put("/:id", authenticate, isValidId, validateBody(schemas.changeSchema), ctrl.updateLot);
+router.put("/:id", isValidId, validateBody(schemas.changeSchema), ctrl.updateLot);
 
-router.delete("/:id", authenticate, isValidId, ctrl.removeLot);
+router.delete("/:id", isValidId, ctrl.removeLot);
 
-router.patch("/:id/favorite", authenticate, isValidId, validateBody(schemas.schemaUpdateFavorite), ctrl.updateFavoriteLot);
+router.patch("/:id/favorite", isValidId, validateBody(schemas.schemaUpdateFavorite), ctrl.updateFavoriteLot);
 
 // router.patch("/pictures", authenticate, upload.single("picture"), avatarSize, ctrl.updateLotPicture);
 
